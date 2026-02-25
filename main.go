384fdcede7e16c33233722aed3d065ad5632507e
AngryFennec <shellfix@gmail.com>
2026-02-25 20:58:11 +0300

    add nextDateHandler

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 	"os"
 
 	"main.go/pkg/db"
+	"main.go/pkg/server"
 )
-
-var port = 7540
-
-const webDir = "./web"
 
 func main() {
 	logger := log.New(os.Stderr, "todo list", log.LstdFlags)
@@ -21,10 +16,7 @@ func main() {
 		logger.Fatalf("%s", err.Error())
 	}
 
-	http.Handle("/", http.FileServer(http.Dir(webDir)))
-	log.Printf("Running server on %d", port)
-
-	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err = server.RunServer()
 	if err != nil {
 		logger.Fatalf("%s", err.Error())
 	}
---
+++ pkg/server/server.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"main.go/pkg/api"
+)
+
+var port = 7540
+
+const webDir = "./web"
+
+func RunServer() error {
+	//	http.Handle("/", http.FileServer(http.Dir(webDir)))
+	log.Printf("Running server on %d", port)
+	api.Init()
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
